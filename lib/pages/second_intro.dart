c8b1c134af321333d410381cc2029cb44e8c9727
jaeyounggg <dlwodud5914@naver.com>
2024-07-20 06:28:29 +0900

    UI Update

--- lib/pages/second_intro.dart
+++ lib/pages/second_intro.dart
@@ -14,66 +14,73 @@ class _SecondIntroState extends State<SecondIntro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        leading: IconButton(
+          padding: const EdgeInsets.only(left: 12.0),
+          icon: const Icon(Icons.arrow_back, color: Colors.black), // 뒤로가기 아이콘 색상 변경
+          onPressed: () => Navigator.of(context).pop(), // 뒤로가기 기능
+        ),
+      ),
       backgroundColor: Colors.grey[300],
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // sub title
-              Text(
-                '식단의 탄단지 비율을 선택하세요',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.grey[800],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 60,
+            ),
+            Text(
+              '식단의 탄단지 비율을 선택하세요',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.grey[800],
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(
+              height: 60,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildToggleButton(0, 'Button 1'),
+                _buildToggleButton(1, 'Button 2'),
+                _buildToggleButton(2, 'Button 3'),
+              ],
+            ),
+
+            const SizedBox(
+              height: 60,
+            ),
+
+            // start now button
+            GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MainPage(),
+                  )),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                textAlign: TextAlign.center,
-              ),
-
-              const SizedBox(
-                height: 60,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildToggleButton(0, 'Button 1'),
-                  _buildToggleButton(1, 'Button 2'),
-                  _buildToggleButton(2, 'Button 3'),
-                ],
-              ),
-
-              const SizedBox(
-                height: 80,
-              ),
-
-              // start now button
-              GestureDetector(
-                onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const MainPage(),
-                    )),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[900],
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  padding: const EdgeInsets.all(25),
-                  child: const Center(
-                    child: Text(
-                      'Start Now',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
+                padding: const EdgeInsets.all(25),
+                child: const Center(
+                  child: Text(
+                    'Start Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -81,7 +88,7 @@ class _SecondIntroState extends State<SecondIntro> {
 
   Widget _buildToggleButton(int index, String text) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
+      padding: const EdgeInsets.symmetric(vertical: 20),
       child: ElevatedButton(
         onPressed: () {
           setState(() {
@@ -89,7 +96,7 @@ class _SecondIntroState extends State<SecondIntro> {
           });
         },
         style: ElevatedButton.styleFrom(
-          padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 24),
+          padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 26),
           foregroundColor: Colors.white,
           textStyle: const TextStyle(
             fontWeight: FontWeight.bold,
