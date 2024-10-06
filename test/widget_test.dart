c50740ff95303546da5acdc07fdd75b01f981ea4
jaeyounggg <dlwodud5914@naver.com>
2024-10-07 02:58:47 +0900

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:diet_macro/services/isar.service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,14 @@ import 'package:diet_macro/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
+    await IsarService.initialize();
+
+    final targetCalories = await IsarService().getTargetData();
+
+    await tester.pumpWidget(MyApp(
+      targetCalories: targetCalories,
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
