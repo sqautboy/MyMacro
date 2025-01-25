d38cd2e45cc7b1c87aeeb8b700d6d24bb463adde
jaeyounggg <dlwodud5914@naver.com>
2025-01-25 21:47:04 +0900

    fix locator

--- lib/core/injection/get_it.dart
+++ lib/core/injection/get_it.dart
@@ -1,9 +1,9 @@
 import 'package:diet_macro/data/datasources/local/isar_datasource.dart';
 import 'package:diet_macro/data/repositories/daily_data/daily_data_repository_impl.dart';
-import 'package:diet_macro/data/repositories/food_search/i_food_repository.dart';
+import 'package:diet_macro/data/repositories/food_search/food_repository.dart';
 import 'package:diet_macro/data/repositories/food_search/food_repository_impl.dart';
 
-import 'package:diet_macro/data/repositories/target_data/i_target_data_repository.dart';
+import 'package:diet_macro/data/repositories/target_data/target_data_repository.dart';
 import 'package:diet_macro/data/repositories/target_data/target_data_repository_impl.dart';
 
 import 'package:diet_macro/presentation/views/diet_view/diet_view_model.dart';
@@ -11,7 +11,7 @@ import 'package:diet_macro/presentation/views/food_search/food_search_viewmodel.
 import 'package:get_it/get_it.dart';
 
 import '../../data/datasources/remote/food_search_api.dart';
-import '../../data/repositories/daily_data/i_daily_data_repository.dart';
+import '../../data/repositories/daily_data/daily_data_repository.dart';
 
 final getIt = GetIt.instance;
 
@@ -21,9 +21,9 @@ void setupDependencies() {
   getIt.registerLazySingleton<IsarDatasource>(() => IsarDatasource());
 
   // Repository
-  getIt.registerLazySingleton<IFoodRepository>(() => FoodRepositoryImpl(getIt<FoodSearchApi>()));
-  getIt.registerLazySingleton<IDailyDataRepository>(() => DailyDataRepositoryImpl(getIt<IsarDatasource>()));
-  getIt.registerLazySingleton<ITargetDataRepository>(() => TargetDataRepositoryImpl(getIt()));
+  getIt.registerLazySingleton<FoodRepository>(() => FoodRepositoryImpl(getIt<FoodSearchApi>()));
+  getIt.registerLazySingleton<DailyDataRepository>(() => DailyDataRepositoryImpl());
+  getIt.registerLazySingleton<TargetDataRepository>(() => TargetDataRepositoryImpl());
 
   // ViewModel
   getIt.registerFactory<FoodSearchViewModel>(() => FoodSearchViewModel());
