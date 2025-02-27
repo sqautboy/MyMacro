3aa6ad9957d2f98f163f45f411154935ad87c6df
jaeyounggg <dlwodud5914@naver.com>
2025-02-28 01:37:19 +0900

    additional DI

--- lib/core/injection/get_it.dart
+++ lib/core/injection/get_it.dart
@@ -1,17 +1,20 @@
-import '../../data/datasources/local/isar_datasource.dart';
-import '../../data/repositories/daily_data/daily_data_repository_impl.dart';
-import '../../data/repositories/food_search/food_repository.dart';
-import '../../data/repositories/food_search/food_repository_impl.dart';
+import '../../data/repository/meal_data/meal_data_repository.dart';
+import '../../data/repository/meal_data/meal_data_repository_impl.dart';
 
-import '../../data/repositories/target_data/target_data_repository.dart';
-import '../../data/repositories/target_data/target_data_repository_impl.dart';
+import '../../data/datasource/local/isar_datasource.dart';
+import '../../data/repository/daily_data/daily_data_repository_impl.dart';
+import '../../data/repository/food_search/food_repository.dart';
+import '../../data/repository/food_search/food_repository_impl.dart';
+
+import '../../data/repository/target_data/target_data_repository.dart';
+import '../../data/repository/target_data/target_data_repository_impl.dart';
 
 import '../../presentation/views/diet_view/diet_view_model.dart';
 import '../../presentation/views/food_search/food_search_viewmodel.dart';
 import 'package:get_it/get_it.dart';
 
-import '../../data/datasources/remote/food_search_api.dart';
-import '../../data/repositories/daily_data/daily_data_repository.dart';
+import '../../data/datasource/remote/food_search_api.dart';
+import '../../data/repository/daily_data/daily_data_repository.dart';
 
 final getIt = GetIt.instance;
 
@@ -24,6 +27,7 @@ void setupDependencies() {
   getIt.registerLazySingleton<FoodRepository>(() => FoodRepositoryImpl(getIt<FoodSearchApi>()));
   getIt.registerLazySingleton<DailyDataRepository>(() => DailyDataRepositoryImpl());
   getIt.registerLazySingleton<TargetDataRepository>(() => TargetDataRepositoryImpl());
+  getIt.registerLazySingleton<MealDataRepository>(() => MealDataRepositoryImpl());
 
   // ViewModel
   getIt.registerFactory<FoodSearchViewModel>(() => FoodSearchViewModel());
